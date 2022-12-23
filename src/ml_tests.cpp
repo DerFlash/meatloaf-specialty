24d9d596ff7a11d33de0c32762648be6496a64a6
firt last <qus123@gmail.com>
2022-12-23 10:05:33 +0100

    commit shit

--- src/ml_tests.cpp
+++ src/ml_tests.cpp
@@ -688,7 +688,7 @@ void runTestsSuite() {
     fnSystem.delay_microseconds(pdMS_TO_TICKS(5000)); // 5sec after connect
 
     //commodoreServer();
-    //seekTest();
+    seekTest();
 
     // ====== Per FS dir, read and write region =======================================
 
