a51dbf13b318f0f8bf447ba1bc14f2b90c1373dc
ssuukk <ssuukk2@gmail.com>
2023-07-10 19:32:01 +0200

    Getting closer - a lot of errors cleared

--- lib/meatloaf/archive/7z.h
+++ lib/meatloaf/archive/7z.h
@@ -1,5 +1,6 @@
 
 // https://stackoverflow.com/questions/22543179/how-to-use-libarchive-properly
+// https://libarchive.org/
 
 #ifndef MEATLOAF_ARCHIVE_7Z
 #define MEATLOAF_ARCHIVE_7Z
@@ -19,22 +20,72 @@
 
 #include "meat_io.h"
 
+// TODO: check how we can use archive_seek_callback, archive_passphrase_callback etc. to our benefit!
+
+/* Returns pointer and size of next block of data from archive. */
+// The read callback returns the number of bytes read, zero for end-of-file, or a negative failure code as above. 
+// It also returns a pointer to the block of data read.
+ssize_t myRead(struct archive *a, void *__src_stream, const void **buff)
+{
+    MStream *src_str = (MStream*)__src_stream;
+
+    // OK, so:
+    // 1. we have to call srcStr.read(...)
+    // 2. set *buff to the bufer read in 1.
+    // 3. return read bytes count
+
+    // *buff = srcBuffer;
+    // return src_str->read(srcBuffer, buffSize);
+    return -1;
+}
+
+int myclose(struct archive *a, void *__src_stream)
+{
+    MStream *src_str = (MStream*)__src_stream;
+
+    // do we want to close srcStream here???
+    return (ARCHIVE_OK);
+}
+
+/* 
+It must return the number of bytes actually skipped, or a negative failure code if skipping cannot be done.
+It can skip fewer bytes than requested but must never skip more.
+Only positive/forward skips will ever be requested.
+If skipping is not provided or fails, libarchive will call the read() function and simply ignore any data that it does not need.
+
+* Skips at most request bytes from archive and returns the skipped amount.
+* This may skip fewer bytes than requested; it may even skip zero bytes.
+* If you do skip fewer bytes than requested, libarchive will invoke your
+* read callback and discard data as necessary to make up the full skip.
+*/
+int64_t myskip(struct archive *a, void *__src_stream, int64_t request)
+{
+    MStream *src_str = (MStream*)__src_stream;
+    if(src_str->isOpen()) {
+        bool rc = src_str->seek(request, SEEK_CUR);
+        return (rc) ? request : ARCHIVE_WARN;
+    }
+    else {
+        return ARCHIVE_FATAL;
+    }
+}
+
+
 /********************************************************
  * Streams implementations
  ********************************************************/
 
-class ArchiveStream: MStream {
+class ArchiveStream: public MStream {
     struct archive *a;
     bool is_open = false;
-    MStream* srcStream = nullptr; // a stream that is able to serve bytes of this archive
     size_t buffSize = 4096;
     uint8_t* srcBuffer = nullptr;
     uint32_t _position = 0;
 
 public:
 
-    ArchiveStream(MStream* srcStream): srcStr(srcStream) {
-        this->srcStr = srcStr;
+    ArchiveStream(std::shared_ptr<MStream> srcStr) {
+        srcStream = srcStr;
         a = archive_read_new();
         archive_read_support_filter_all(a);
         archive_read_support_format_all(a);
@@ -51,7 +102,7 @@ public:
         if(!is_open) {
             // callbacks set here:
             //                                    open, read  , skip,   close
-            int r = archive_read_open2(a, srcStr, NULL, myread, myskip, myclose);
+            int r = archive_read_open2(a, srcStream, NULL, myRead, myskip, myclose);
             if (r == ARCHIVE_OK)
                 is_open = true;
         }
@@ -77,12 +128,17 @@ public:
         return r;
     }
 
+    uint32_t write(const uint8_t *buf, uint32_t size) override {
+        return -1;
+    }
+
     // For files with a browsable random access directory structure
     // d64, d74, d81, dnp, etc.
     bool seekPath(std::string path) override {
         struct archive_entry *entry;
         while (archive_read_next_header(a, &entry) == ARCHIVE_OK) {
-            if(mstr::compare(path, archive_entry_pathname(entry)))
+            std::string entryName = archive_entry_pathname(entry);
+            if(mstr::compare(path, entryName))
                 return true;
         }
         return false;
@@ -107,57 +163,25 @@ public:
         return 0; // whatever...
     }
 
+    virtual uint32_t size() override {
+        return -1;
+    }
+
+    virtual size_t error() override {
+        // huh???
+        return -1;
+    }
+
+    virtual bool seek(uint32_t pos) override {
+        return false;
+    }
+
 protected:
-    MStream* srcStr;
+    std::shared_ptr<MStream> srcStream = nullptr; // a stream that is able to serve bytes of this archive
 
 private:
-    // TODO: check how we can use archive_seek_callback, archive_passphrase_callback etc. to our benefit!
 
-    /* Returns pointer and size of next block of data from archive. */
-    // The read callback returns the number of bytes read, zero for end-of-file, or a negative failure code as above. 
-    // It also returns a pointer to the block of data read.
-    ssize_t myread(struct archive *a, void *__src_stream, const void **buff)
-    {
-        //MStream *src_str = __src_stream; // no need, we have it as a field
 
-        // OK, so:
-        // 1. we have to call srcStr.read(...)
-        // 2. set *buff to the bufer read in 1.
-        // 3. return read bytes count
-
-        *buff = srcBuffer;
-        return srcStr->read(srcBuffer, buffSize);
-    }
-
-    int myclose(struct archive *a, void *__src_stream)
-    {
-        //MStream *src_str = __src_stream; // no need, we have it as a field
-
-        // do we want to close srcStream here???
-        return (ARCHIVE_OK);
-    }
-
-    /* 
-    It must return the number of bytes actually skipped, or a negative failure code if skipping cannot be done.
-    It can skip fewer bytes than requested but must never skip more.
-    Only positive/forward skips will ever be requested.
-    If skipping is not provided or fails, libarchive will call the read() function and simply ignore any data that it does not need.
-
-    * Skips at most request bytes from archive and returns the skipped amount.
-    * This may skip fewer bytes than requested; it may even skip zero bytes.
-    * If you do skip fewer bytes than requested, libarchive will invoke your
-    * read callback and discard data as necessary to make up the full skip.
-    */
-    int myskip(struct archive *a, void *client_data, la_int64_t request)
-    {
-        if(is_open) {
-            bool rc = srcStr->seek(request, SEEK_CUR);
-            return (rc) ? request : ARCHIVE_WARN;
-        }
-        else {
-            return ARCHIVE_FATAL;
-        }
-    }
 
 };
 
@@ -202,7 +226,7 @@ public:
                 auto newFile = MFSOwner::File(archive_entry_pathname(entry));
                 // TODO - we can probably fill newFile with some info that is
                 // probably available in archive_entry structure!
-                newFile->size(entry->size); // etc.
+                newFile->size(entry->ae_stat.aest_size); // etc.
 
                 return newFile;
             }
@@ -228,7 +252,7 @@ private:
         a = archive_read_new();
         archive_read_support_filter_all(a);
         archive_read_support_format_all(a);
-        int r = archive_read_open2(a, srcStr, NULL, myread, myskip, myclose);
+        int r = archive_read_open2(a, srcStr, NULL, myRead, myskip, myclose);
         if (r == ARCHIVE_OK) {
             return true;
         }
